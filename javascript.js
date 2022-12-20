ce06a3260439853f8a6b7b8a0652f0db06de00d7
Kyle Angelich <kyleangelich1@gmail.com>
2022-12-20 01:56:11 -0800

    Add getRandomInt function to be used for generating computer's move/choice

--- javascript.js
+++ javascript.js
@@ -1,2 +1,10 @@
-function getComputerChoice () {
+// function getComputerChoice () {
+// }
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
 }
+
+console.log(getRandomInt(0, 3));
