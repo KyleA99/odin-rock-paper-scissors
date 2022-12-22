f70269e0c8a9d57b6fb5d7a1cec9fc5947d80225
Kyle Angelich <kyleangelich1@gmail.com>
2022-12-21 19:23:28 -0800

    Add an array to add values for rock, paper, and scissor choice classes

--- javascript.js
+++ javascript.js
@@ -1,10 +1,18 @@
 // function getComputerChoice () {
 // }
 
+const choiceClass = []
+    choiceClass [0] = "Rock";
+    choiceClass [1] = "Paper";
+    choiceClass [2] = "Scissors";
+    const randomChoiceClass = choiceClass[(Math.random() * choiceClass) | 0]
+
+    console.log(randomChoiceClass, choiceClass[randomChoiceClass])
+
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
 }
-
 console.log(getRandomInt(0, 3));
