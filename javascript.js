da5d79458a3cfe582fcd09b666c4f93b8f921e7d
Kyle Angelich <kyleangelich1@gmail.com>
2022-12-22 14:21:51 -0800

    Unconfirmed to be correct - Add conditional statements to determine round results

--- javascript.js
+++ javascript.js
@@ -16,7 +16,26 @@
     getComputerChoice();
 
 // declares a function named roundResults which determines if the player or computer won an individual round
+// add conditional if, else if, and else statements to return a string that declares the winner of the round
 
 function roundResults (playerSelection, computerSelection){
-
+    if (playerSelection == "Rock" && computerSelection == "Rock") {
+        message = "Tie!";
+    } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        message = "You Lose!";
+    } else if ((playerSelection == "Rock" && computerSelection == "Scissors")) {
+        message = "You Win!";
+    } else if (playerSelection == "Paper" && computerSelection == "Paper") {
+        message = "Tie!";
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        message = "You Lose!";
+    } else if ((playerSelection == "Paper" && computerSelection == "Rock")) {
+        message = "You Win!";
+    } else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
+        message = "Tie!";
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        message = "You Lose!";
+    } else {
+        message = "You Win!";
+    }
 }
