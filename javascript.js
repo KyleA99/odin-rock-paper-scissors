199a3b5347f2fdaa7860426ecc2876a46603d81d
Kyle Angelich <kyleangelich1@gmail.com>
2022-12-22 13:30:17 -0800

    Reformat getComputerChoice function so that console.log actually logs function result to console

--- javascript.js
+++ javascript.js
@@ -1,10 +1,15 @@
-// function getComputerChoice () {
-// }
-
-const choiceClass = []
-    choiceClass [0] = "Rock";
-    choiceClass [1] = "Paper";
-    choiceClass [2] = "Scissors";
-    const randomChoiceClass = Math.floor(Math.random() * choiceClass.length);
-
-    console.log(randomChoiceClass, choiceClass[randomChoiceClass])
+ // declares a function named getComputerChoice> declares an array named choiceClass, with contains an item for each
+ // computer choice> randomly selects a number which corresponds to an array item> logs message to console which displays
+ // randomly generated number choice and corresponding computer choice> ends function execution and specifies value to
+ // be returned to the function caller
+ 
+ function getComputerChoice () {
+    const choiceClass = []
+        choiceClass [0] = "Rock";
+        choiceClass [1] = "Paper";
+        choiceClass [2] = "Scissors";
+        const randomChoiceClass = Math.floor(Math.random() * choiceClass.length);
+        console.log(randomChoiceClass, choiceClass[randomChoiceClass]);
+        return randomChoiceClass;
+ }
+    getComputerChoice();
