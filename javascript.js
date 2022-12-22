d4b4d557f239283390e45d39f9597b788b612934
Kyle Angelich <kyleangelich1@gmail.com>
2022-12-21 19:31:58 -0800

    Delete previous function relating to Math.random choice selection

--- javascript.js
+++ javascript.js
@@ -8,11 +8,3 @@ const choiceClass = []
     const randomChoiceClass = Math.floor(Math.random() * choiceClass.length);
 
     console.log(randomChoiceClass, choiceClass[randomChoiceClass])
-
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-}
-console.log(getRandomInt(0, 3));
