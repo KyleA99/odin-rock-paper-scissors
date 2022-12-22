d868d1c8edbe66755988c09d4d7e2cf07210361c
Kyle Angelich <kyleangelich1@gmail.com>
2022-12-21 19:30:36 -0800

    Revise array expression to provide results other than 0/rock

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ const choiceClass = []
     choiceClass [0] = "Rock";
     choiceClass [1] = "Paper";
     choiceClass [2] = "Scissors";
-    const randomChoiceClass = choiceClass[(Math.random() * choiceClass) | 0]
+    const randomChoiceClass = Math.floor(Math.random() * choiceClass.length);
 
     console.log(randomChoiceClass, choiceClass[randomChoiceClass])
 
