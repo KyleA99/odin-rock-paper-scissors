2a653b947bf5694111df35ff15e06ae20cd970c1
Kyle Angelich <kyleangelich1@gmail.com>
2022-12-22 13:33:57 -0800

    Edit console.log to not include randomly selected number, only the corresponding computer choice

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
- // declares a function named getComputerChoice> declares an array named choiceClass, with contains an item for each
- // computer choice> randomly selects a number which corresponds to an array item> logs message to console which displays
- // randomly generated number choice and corresponding computer choice> ends function execution and specifies value to
- // be returned to the function caller
+ // declares a function named getComputerChoice
+ // declares an array named choiceClass, with contains an item for each computer choice
+ // randomly selects a number which corresponds to an array item
+ // logs message to console which displays randomly generated computer choice
+ // ends function execution and specifies value to be returned to the function caller
  
  function getComputerChoice () {
     const choiceClass = []
@@ -9,7 +10,7 @@
         choiceClass [1] = "Paper";
         choiceClass [2] = "Scissors";
         const randomChoiceClass = Math.floor(Math.random() * choiceClass.length);
-        console.log(randomChoiceClass, choiceClass[randomChoiceClass]);
+        console.log(choiceClass[randomChoiceClass]);
         return randomChoiceClass;
  }
     getComputerChoice();
