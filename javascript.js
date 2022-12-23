dc2546366215a84fa306337c25db28837dbffccd
Kyle Angelich <kyleangelich1@gmail.com>
2022-12-23 15:14:33 -0800

    Unconfirmed to be correct - add return/console log statements

--- javascript.js
+++ javascript.js
@@ -5,37 +5,39 @@
  // ends function execution and specifies value to be returned to the function caller
  
  function getComputerChoice () {
-    const choiceClass = []
+    let choiceClass = []
         choiceClass [0] = "Rock";
         choiceClass [1] = "Paper";
         choiceClass [2] = "Scissors";
-        const randomChoiceClass = Math.floor(Math.random() * choiceClass.length);
-        console.log(choiceClass[randomChoiceClass]);
+        let randomChoiceClass = Math.floor(Math.random() * choiceClass.length);
+        // console.log(choiceClass[randomChoiceClass]);
         return randomChoiceClass;
  }
     getComputerChoice();
 
 // declares a function named roundResults which determines if the player or computer won an individual round
-// add conditional if, else if, and else statements to return a string that declares the winner of the round
+// add conditional statements to return a string that declares the winner of the round
+// declares function parameters
 
 function roundResults (playerSelection, computerSelection){
-    if (playerSelection == "Rock" && computerSelection == "Rock") {
+    if (playerSelection === computerSelection) {
         message = "Tie!";
     } else if (playerSelection == "Rock" && computerSelection == "Paper") {
         message = "You Lose!";
-    } else if ((playerSelection == "Rock" && computerSelection == "Scissors")) {
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
         message = "You Win!";
-    } else if (playerSelection == "Paper" && computerSelection == "Paper") {
-        message = "Tie!";
     } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
         message = "You Lose!";
-    } else if ((playerSelection == "Paper" && computerSelection == "Rock")) {
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
         message = "You Win!";
-    } else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
-        message = "Tie!";
     } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
         message = "You Lose!";
     } else {
         message = "You Win!";
     }
+    computerSelection = getComputerChoice();
+    playerSelection = "Rock";
+    console.log(message);
+    return;
 }
+    roundResults();
