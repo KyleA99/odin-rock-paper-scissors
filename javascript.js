f2f59e8882a674b07e4fe513bce3b0237ed38aab
Kyle Angelich <kyleangelich1@gmail.com>
2022-12-25 16:32:47 -0800

    Add return variable to roundResults function for the winner

--- javascript.js
+++ javascript.js
@@ -15,21 +15,27 @@
 function roundResults (playerSelection, computerSelection){
     if (playerSelection === computerSelection) {
         message = "Tie!";
+        winner = "Tie"
     } else if (playerSelection == "rock" && computerSelection == "paper") {
         message = "You Lose!";
+        winner = "Computer";
     } else if (playerSelection == "rock" && computerSelection == "cissors") {
         message = "You Win!";
+        winner = "Player";
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         message = "You Lose!";
+        winner = "Computer";
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         message = "You Win!";
+        winner = "Player";
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         message = "You Lose!";
+        winner = "Computer";
     } else {
         message = "You Win!";
+        winner = "Player";
     }
-    console.log(message);
-    return;
+    return winner;
 }
     playerSelection = "paper";
     computerSelection = getComputerChoice();
