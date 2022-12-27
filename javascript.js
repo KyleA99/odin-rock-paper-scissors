75c20446a94acbad03e7d2e0fa6711052cb10901
Kyle Angelich <kyleangelich1@gmail.com>
2022-12-26 16:05:39 -0800

    Declare variable to track number of wins

--- javascript.js
+++ javascript.js
@@ -48,6 +48,7 @@ function playRound (playerSelection, computerSelection){
 // 5 round game
 
 function game () {
+    let numberWins = 0;
     for (let i = 0; i < 5; ++i)
     {
         // body of the loop
