04fd6054a7ae0dc077dbaf71f4934fc4581aa2c1
Kyle Angelich <kyleangelich1@gmail.com>
2022-12-30 16:17:03 -0800

    Omit game() function and restructure playRound() function to fix scoping issues

--- javascript.js
+++ javascript.js
@@ -6,7 +6,6 @@ function getComputerChoice () {
         let randomChoiceClass = Math.floor(Math.random() * choiceClass.length);
         return choiceClass[randomChoiceClass];
  }
-getComputerChoice();
 
 function getPlayerChoice () {
     return prompt("Please choose either rock, paper, or scissors.");
@@ -14,54 +13,54 @@ function getPlayerChoice () {
 
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        roundResults = "Tie!";
-        winner = "Tie";
+        return "Tie!";
     } else if (playerSelection == "rock" && computerSelection == "paper") {
-        roundResults = "You lost this round!";
-        winner = "Computer";
+        return "You lost this round!";
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        roundResults = "You won this round!";
-        winner = "Player";
+        return "You won this round!";
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        roundResults = "You lost this round!";
-        winner = "Computer";
+        return "You lost this round!";
     } else if (playerSelection == "paper" && computerSelection == "rock") {
-        roundResults = "You won this round!";
-        winner = "Player";
+        return "You won this round!";
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        roundResults = "You lost this round!";
-        winner = "Computer";
+        return "You lost this round!";
     } else {
-        roundResults = "You won this round!";
-        winner = "Player";
+        return "You won this round!";
     }
-
-    function game () {
-        let playerScore = 0;
-        let computerScore = 0;
-        for (let i = 0; i < 5; ++i) {
-        playerSelection = getPlayerChoice().toLowerCase();
-        computerSelection = getComputerChoice().toLowerCase();
-                if (winner === "Player") {
-                    ++playerScore;
-                } else if (winner === "Computer") {
-                    ++computerScore;
-                } else {
-                    // Do nothing.
-                }
-        }
-        if (playerScore > computerScore) {
-            gameResults = (`${playerScore}, ${computerScore}: You won the game!`);
-        } else if (playerScore < computerScore) {
-            gameResults = (`${playerScore}, ${computerScore}: You lost the game!`);
-        } else {
-            gameResults = (`${playerScore}, ${computerScore}: You tied, please play again!`);
-            }
-        }
-    game();
 }
-playerSelection = getPlayerChoice();
-computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice().toLowerCase();
+const computerSelection = getComputerChoice().toLowerCase();
+console.log(playRound(playerSelection, computerSelection));
 
-playRound(playerSelection, computerSelection);
-console.log(gameResults)
+
+
+// function game () {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     for (let i = 0; i < 1; ++i) {
+//     playerSelection = getPlayerChoice();
+//     computerSelection = getComputerChoice();
+//             if (winner === "Player") {
+//                 ++playerScore;
+//             } else if (winner === "Computer") {
+//                 ++computerScore;
+//             } else {
+//                 // Do nothing.
+//             }
+//         }
+//         if (playerScore > computerScore) {
+//             gameResults = (`${playerScore}, ${computerScore}: You won the game!`);
+//         } else if (playerScore < computerScore) {
+//             gameResults = (`${playerScore}, ${computerScore}: You lost the game!`);
+//         } else {
+//             gameResults = (`${playerScore}, ${computerScore}: You tied, please play again!`);
+//             }
+//         }
+//     game();
+// playerSelection = getPlayerChoice();
+
+// playRound(playerSelection, computerSelection);
+// console.log(gameResults)
+
+
+//do not nest playRound() and game()
